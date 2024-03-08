edac7e5b265d8da0c0ed347626d12c6854911e46
AqilHassan22 <aqilali155@gmail.com>
2024-03-08 13:31:45 +0100

    changes

--- easy.md
+++ easy.md
@@ -3,3 +3,19 @@
 # Theory
 
 # Methods
+
+| Right | Left | Default | Center |
+|------:|:-----|---------|:------:|
+|    12 | 12   | 12      |   12   |
+|   123 | 123  | 123     |  123   |
+|     1 | 1    | 1       |   1    |
+
+Inline equations $x=f (N,K)$
+
+$$
+Y=C+I+G
+$$
+
+$$
+123=\frac(\frac)(\theta)
+$$
--- todos.R
+++ todos.R
@@ -1,2 +1,20 @@
 # Todos
 2+2
+#C1
+numbers <- c(4,5,8,11)
+sum(numbers)
+#C2
+rnorm(n=100, mean = 0, sd = 1)
+#C3
+now <- as.numeric(substring(paste(Sys.Date()),1,4))
+started <- 2020
+born <- 1995
+spent <- (now-started)/(now-born)*100
+round(spent, digits=4)
+
+
+
+```{r}
+Library KableExtra:
+kableExtra kable:iris
+```
